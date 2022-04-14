541b895c4dd154aa61ad2a5ad5b55d8112b9ba32
fazlullahb0 <fazlullahb0@gmail.com>
2022-04-14 14:54:31 +0530

    flask

--- basic.py
+++ basic.py
@@ -2,9 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #127.0.0.1:5000
 def index():
     return '<h1> Hello Faiz </h1>'
+
+@app.route('/information') #127.0.0.1:5000/information
+def info():
+    return "<h1>Flask Lecture for YouTube </h1>"
+
+@app.route('/user/<name>')
+def user(name):
+    return "<h1> This is a page for {}</h1>".format(name.title())
 
 
 if __name__ == '__main__':
